ee666db7164e5c4e084bda6d1bd810ddb1f3f5a5
Joe Morrissey <jrm15@illinois.edu>
2023-12-04 21:46:52 -0600

    Added MatchOrder Functionality

--- OrderBook.ts
+++ OrderBook.ts
@@ -36,6 +36,27 @@ class Order {
         this.prevOrder = prevOrder; 
         this.parentLimit = parentLimit;
     }
+
+    // Method to check if the order can be matched with another order
+    canMatchWith(otherOrder: Order): boolean {
+        // Check if the orders are of opposite types (buy and sell)
+        return (
+            this.currentOrder !== otherOrder.currentOrder && // Assuming orderType is 'buy' or 'sell'
+            ((this.currentOrder === 'buy' && this.limit >= otherOrder.limit) || // For buy order
+            (this.currentOrder === 'sell' && this.limit <= otherOrder.limit)) // For sell order
+        );
+    }
+
+    // Method to execute a matched order
+    executeMatchedOrder(otherOrder: Order): void {
+        // Perform order execution actions here
+        // For example, updating order quantities, executing trades, etc.
+        // This part would involve your business logic for executing trades
+        console.log(`Matched order ${this.idNumber} with order ${otherOrder.idNumber}`);
+        // Update order quantities, execute trade, etc.
+    }
+
+    
 }
 class Limit {
     limitPrice: number;
@@ -126,8 +147,61 @@ class Book {
         limitNode.totalVolume += order.shares;
     }
 
+    findOrder(id: number): void {
+
+    }
+
+    removeOrder(id: number): void {
+
+    }
+
     matchOrder(): void {
         // TODO: implement matchOrder to check for trades
+        if (!this.buyTree || !this.sellTree) {
+            // Handle empty trees or invalid state
+            return;
+        }
+
+        let buyLimit = this.highestBuy;
+        let sellLimit = this.lowestSell;
+
+        while (buyLimit && sellLimit && buyLimit.limitPrice >= sellLimit.limitPrice) {
+            // Loop until no more compatible orders can be found
+            let buyOrder = buyLimit.headOrder;
+            let sellOrder = sellLimit.headOrder;
+
+            while (buyOrder && sellOrder) {
+                if (buyOrder.canMatchWith(sellOrder)) {
+                    // Execute matched orders
+                    buyOrder.executeMatchedOrder(sellOrder);
+
+                    // Update quantities, remove orders, etc.
+                    // (This part would depend on your implementation)
+
+                    // Move to the next orders at these limits
+                    buyOrder = buyOrder.nextOrder;
+                    sellOrder = sellOrder.nextOrder;
+                } else {
+                    // Move to the next order
+                    if (buyOrder.entryTime < sellOrder.entryTime) {
+                        buyOrder = buyOrder.nextOrder;
+                    } else {
+                        sellOrder = sellOrder.nextOrder;
+                    }
+                }
+            }
+
+            // Move to the next limits
+            if (buyLimit.limitPrice === sellLimit.limitPrice) {
+                buyLimit = buyLimit.parent || null;
+                sellLimit = sellLimit.parent || null;
+            } else if (buyLimit.limitPrice > sellLimit.limitPrice) {
+                sellLimit = sellLimit.rightChild || null;
+            } else {
+                buyLimit = buyLimit.leftChild || null;
+            }
+        } 
+
     }
 }
 
