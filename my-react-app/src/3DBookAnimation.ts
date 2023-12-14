c0e8b2e8b0f8cab8a47d2b2571750540606cb2b7
Joe Morrissey <jrm15@illinois.edu>
2023-12-13 22:31:17 -0600

    Added Create Function to 3D Animation

--- my-react-app/src/3DBookAnimation.ts
+++ my-react-app/src/3DBookAnimation.ts
@@ -13,12 +13,21 @@ export class BookAnimation {
     public cameraManager: CameraManager;
     public priceLevelManager: PriceLevelManager;
     public instrumentRepository: InstrumentRepository; 
+    private depth: number;
+    private numTicks: number;
+    private ticksPerLabel = 20;
+    private labelsPerSide = 0;
+    private box: Object3D<Mesh, MeshLambertMaterial> | undefined;
+    private totalLabels = 0;
+    private writtenText: [];
     // Other private properties and dependencies
 
     constructor(scene: Scene, camera: PerspectiveCamera, book: OrderBook, numTicks: number, depth: number, rendererDomElement: HTMLElement) {
         this.sceneManager = new SceneManager(scene);
         this.cameraManager = new CameraManager(camera, rendererDomElement, depth);
         this.priceLevelManager = new PriceLevelManager(depth, numTicks);
+        this.depth = depth;
+        this.numTicks = numTicks;
         this.setup();  
         // Initialize other properties
     }
@@ -29,12 +38,45 @@ export class BookAnimation {
     }
     // Methods to manage the animation
 
+    // create(): void {
+    //     // Logic to create the animation
+    // }
+
+    // Create functionality:
     create(): void {
-        // Logic to create the animation
+        this.createBox();
+        this.createLabels();
+        // this.reset();
+    }
+    
+    createBox() {
+        const boxGeometry = new Mesh(this.depth, 1, 1);
+        const boxMaterial = new MeshLambertMaterial({ color: 0xffffff });
+        this.box = new Object3D(boxGeometry, boxMaterial, 2 * this.numTicks * this.depth);
+        this.sceneManager.addElement(this.box);
+        this.setColor(Color(0xF0D0C9));
+    }
+
+    
+    createLabels() {
+        this.labelsPerSide = Math.floor(this.numTicks / this.ticksPerLabel);
+        this.totalLabels = 1 + 2 * this.labelsPerSide;
+    
+        // Add text to the label
+    }
+      
+    setColor(color: Color) {
+        for (let i = 0; i < 2 * this.depth * this.numTicks; i++) {
+            this.box.setColorAt(i, Color);
+        }
     }
 
     destroy(): void {
         // Logic to clean up and destroy the animation
+    }
+
+    reset(): void{
+        // Logic to reset the animation
     }
 
     update(): void {
